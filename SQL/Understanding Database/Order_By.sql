2c6f83bd5e91e3555c95959e2a10d6104e5e0073
Anush72 <anushgiri72@gmail.com>
2026-01-16 06:52:07 +1100

    Order by Clause

---
+++ SQL/Understanding Database/Order_By.sql
@@ -0,0 +1,13 @@
+# Order By Clause
+
+# Sort by Single Columns
+SELECT * 
+FROM employees
+order by 
+Salary Desc;
+
+#Sort by Multiple Columns
+SELECT * 
+FROM employees
+order by 
+Salary Desc, Department ASC;
