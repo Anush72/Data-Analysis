d1e951456dd61b1793794c49d7df2dac2aa84888
Anush72 <anushgiri72@gmail.com>
2026-01-19 20:04:47 +1100

    Logical Operator

---
+++ SQL/Understanding Database/Operator_SQL.sql
@@ -0,0 +1,31 @@
+# SQL Logical Operator
+USE chinook;
+
+# AND - Should Satisfy both condition
+SELECT * 
+FROM customer
+where Country = 'Brazil' AND SupportRepId = 3;
+
+# OR - Should Satisfy one Condition
+
+SELECT * 
+FROM  track
+WHERE GenreId = 1 or AlbumId = 1;
+
+# In Operator - Statisfy what inside the bracket
+SELECT * 
+FROM customer
+WHERE Country in ('Austria','Canada');
+
+# NOT Operator - Should return which is not satisfy
+SELECT * 
+FROM customer
+WHERE Country Not in ('Austria','Canada');
+
+
+# Like Operator - Search For Specific Patterns
+
+SELECT * 
+FROM customer
+WHERE FirstName Like 'R%';
+
