d40323d21be676001dc2c795e6c8195f70338614
Anush72 <anushgiri72@gmail.com>
2026-01-21 21:59:46 +1100

    NULL Operator in SQL

---
+++ SQL/Understanding Database/ISNULL_Operator.sql
@@ -0,0 +1,25 @@
+# NULL Operator
+
+
+# NULL with WHERE Clause
+SELECT * 
+FROM customer
+WHERE Fax IS NULL;
+
+# NULL Operator on Multiple Columns
+
+SELECT * 
+FROM customer 
+WHERE Fax is NULL OR Company IS NULL;
+
+# IS NuLL with Count Function
+SELECT count(*) as empty_fax
+FROM customer
+WHERE FAX IS NULL;
+
+# Update Statement with NULL
+UPDATE customer
+SET Fax = 'XXXXXXXX'
+WHERE Fax IS NULL;
+
+# 
