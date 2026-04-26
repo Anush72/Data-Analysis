596b5123ef7695ce7e877354f161137f2de60dd6
Anush72 <anushgiri72@gmail.com>
2026-04-26 13:34:49 +1000

    AFL Data Analysis

---
+++ Projects/AFL-Data-Analysis/SQL/afl_practice.sql
@@ -0,0 +1,16 @@
+-- Counting the total matches
+SELECT 
+count(*) as Total_Matches
+from matches;
+
+-- All the team
+SELECT * 
+FROM teams;
+
+-- Total number of playes
+SELECT
+COUNT(*) as Total_Players
+FROM players;
+
+-- Total goal scored by each players
+
